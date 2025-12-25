67d747659d5d0cc1e1f8f0f873ad1759902b0b77
RAUNAKVERMA22 <raunakverma022@gmail.com>
2025-12-25 21:07:45 +0530

    Added CandyCrazeApp

---
+++ CandyCrazeApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Program
+{
+    // Method to calculate discounted price
+    public Candy CalculateDiscountedPrice(Candy candy)
+    {
+        // Calculate total price
+        double totalPrice = candy.Quantity * candy.PricePerPiece;
+
+        // Assign discount percentage based on flavour
+        if (candy.Flavour == "Strawberry")
+            candy.Discount = 15;
+        else if (candy.Flavour == "Lemon")
+            candy.Discount = 10;
+        else if (candy.Flavour == "Mint")
+            candy.Discount = 5;
+
+        // Calculate discounted price
+        candy.TotalPrice = totalPrice - (totalPrice * candy.Discount / 100);
+
+        return candy;
+    }
+
+    // Main method
+    public static void Main()
+    {
+        Program program = new Program();
+        Candy candy = new Candy();
+
+        Console.WriteLine("Enter the flavour");
+        candy.Flavour = Console.ReadLine();
+
+        Console.WriteLine("Enter the quantity");
+        candy.Quantity = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the price per piece");
+        candy.PricePerPiece = int.Parse(Console.ReadLine());
+
+        if (candy.ValidateCandyFlavour())
+        {
+            candy = program.CalculateDiscountedPrice(candy);
+
+            Console.WriteLine("Flavour : " + candy.Flavour);
+            Console.WriteLine("Quantity : " + candy.Quantity);
+            Console.WriteLine("Price Per Piece : " + candy.PricePerPiece);
+            Console.WriteLine("Total Price : " + (candy.Quantity * candy.PricePerPiece));
+            Console.WriteLine("Discount Price : " + candy.TotalPrice);
+        }
+        else
+        {
+            Console.WriteLine("Invalid flavour");
+        }
+    }
+}
