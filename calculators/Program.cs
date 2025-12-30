b2570b7da75e86bcbe031086b44614a20d4ea62a
RAUNAKVERMA22 <raunakverma022@gmail.com>
2025-12-30 23:14:44 +0530

    added calc app

---
+++ calculators/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter operator (+ - * /): ");
+        char op = Console.ReadLine()[0];
+
+        switch (op)
+        {
+            case '+':
+                Console.WriteLine("Result = " + (a + b));
+                break;
+
+            case '-':
+                Console.WriteLine("Result = " + (a - b));
+                break;
+
+            case '*':
+                Console.WriteLine("Result = " + (a * b));
+                break;
+
+            case '/':
+                if (b != 0)
+                    Console.WriteLine("Result = " + (a / b));
+                else
+                    Console.WriteLine("Division by zero not allowed");
+                break;
+
+            default:
+                Console.WriteLine("Invalid Operator");
+                break;
+        }
+    }
+}
