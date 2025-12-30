8fea46fee27dc2fa42de3aa4df2e9aa48650b8a7
RAUNAKVERMA22 <raunakverma022@gmail.com>
2025-12-30 22:43:38 +0530

    added quadratic equation

---
+++ quadraticeqn/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Quadratic
+{
+    static void Main()
+    {
+        Console.Write("Enter a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Enter b: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("Enter c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        double d = b * b - 4 * a * c;
+
+        if (d > 0)
+            Console.WriteLine("Two real roots");
+        else if (d == 0)
+            Console.WriteLine("One real root");
+        else
+            Console.WriteLine("No real roots");
+    }
+}
+
