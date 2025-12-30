e729688a6ba8c77fdd6db1ba1df0e2b5d54e18bf
RAUNAKVERMA22 <raunakverma022@gmail.com>
2025-12-30 22:53:24 +0530

    added leap year file

---
+++ Leapyear/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class LeapYear
+{
+    static void Main()
+    {
+        Console.Write("Enter year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            Console.WriteLine("Leap Year");
+        else
+            Console.WriteLine("Not a Leap Year");
+    }
+}
+
+
+
+
+
+
+
+
