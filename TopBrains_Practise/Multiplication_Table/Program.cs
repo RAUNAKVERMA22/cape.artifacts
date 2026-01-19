02a3a19d5b6a019aade1c0561d7e24507f52ce34
RAUNAKVERMA22 <raunakverma022@gmail.com>
2026-01-19 11:53:04 +0530

    Multiplication table

---
+++ TopBrains_Practise/Multiplication_Table/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int upto = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= upto ; i++)
+        {
+            Console.Write(n*i);
+            if(i < upto)
+            {
+                Console.Write(" ");
+            }
+        }
+    }
+}
