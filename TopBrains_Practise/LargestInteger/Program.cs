6441995666c7ff009aa17f9b41ac04dab48aba83
RAUNAKVERMA22 <raunakverma022@gmail.com>
2026-01-19 14:57:48 +0530

    largest of 3 num

---
+++ TopBrains_Practise/LargestInteger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter num1: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter num2: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter num3: ");
+        int num3 = int.Parse(Console.ReadLine());
+        if(num1>num2 && num1 > num3)
+        {
+            Console.WriteLine("The largest integer is: ");
+            Console.WriteLine(num1);
+        }
+        else if(num2>num1 && num2 > num3)
+        {
+            Console.WriteLine("The largest integer is: ");
+            Console.WriteLine(num2);
+        }
+        else
+        {
+            Console.WriteLine("The largest integer is: ");
+            Console.WriteLine(num3);
+        }
+    }
+}
