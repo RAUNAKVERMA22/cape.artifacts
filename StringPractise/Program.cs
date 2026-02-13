7ba3c3d296696bc90c15ce952a8fb8468e0ea01b
RAUNAKVERMA22 <raunakverma022@gmail.com>
2026-02-13 14:57:40 +0530

    string practise questions

---
+++ StringPractise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter the string:");
+        string input = Console.ReadLine();
+        //string[] ch = input.Split(' ');
+        input = input.ToLower();
+        string[] words = input.Split(' ');
+        Dictionary<string, int> freq = new Dictionary<string, int>();
+        foreach(var word in words)
+        {
+            if (freq.ContainsKey(word))
+            {
+                freq[word]++;
+            }
+            else
+            {
+                freq[word] = 1;
+            }
+            }
+            Console.WriteLine("frequency of each word:");
+        foreach(var i in freq){
+            Console.WriteLine($"{i.Key} : {i.Value}");
+            
+        }
+        }
+        
+
+    }
